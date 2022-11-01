13aaa289af23cc9cc5459ab01079c749f1dc88ac
dacelibermane <dace.libermane@gmail.com>
2022-11-01 20:17:54 +0200

    ŗemove comments

--- 05-functions/05.php
+++ 05-functions/05.php
@@ -1,10 +1,4 @@
 <?php
-//Create a 2D associative array in 2nd dimension with fruits and their weight.
-//Create a function that determines if fruit has weight over 10kg. Create a secondary
-//array with shipping costs per weight. Meaning that you can say that over 10 kg shipping
-//costs are 2 euros, otherwise its 1 euro. Using foreach loop print out the values of the
-//fruits and how much it would cost to ship this product.
-
 
 $fruits = [
     ["fruit" => "apple", "weight" => 9],
