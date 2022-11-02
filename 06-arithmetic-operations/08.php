9eb320f22137515b05ffb8ccfc97c05b0f74cfbb
dacelibermane <dace.libermane@gmail.com>
2022-11-02 18:47:39 +0200

    change main method

--- 06-arithmetic-operations/08.php
+++ 06-arithmetic-operations/08.php
@@ -1,41 +1,52 @@
 <?php
 
 
+class Employee
+{
+    public float $basePay;
+    public int $hoursWorked;
+    public float $minimumWage = 8.00;
+    public int $maxHours = 60;
+    public float $totalSalary = 0.00;
 
-class Employee{
-    public string $name;
-    public float $base;
-    public int $hours;
 
-    public function __construct($name,$base,$hours){
-            $this->name = $name;
-            $this->base = $base;
-            $this->hours = $hours;
+    public function __construct($base, $hours)
+    {
+        $this->basePay = $base;
+        $this->hoursWorked = $hours;
+    }
+
+
+    public function main()
+    {
+        $this->calculateSalary($this->basePay,$this->hoursWorked,$this->minimumWage,$this->maxHours);
+
+    }
+
+    public function calculateSalary($base,$hours,$minimumWage,$maxHours)
+    {
+        if (($base < $minimumWage) || ($hours > $maxHours)) {
+           echo "Error!";
+        } else {
+            if ($hours > 40) {
+                $totalSalary = $base * 40 + 1.5 * $base * ($hours - 40);
+
+            } else {
+                $totalSalary = $base * $hours;
+            }
+            echo "Your total salary is: " . round($totalSalary, 2);
         }
 
-        public function calculateSalary($name,$base,$hours):string{
-            $this->name = $name;
-            $this->base = $base;
-            $this->hours = $hours;
-            $minimumWage = 8.00;
-            $maxHours = 60;
-            $totalSalary = 0;
-            if(($base < $minimumWage) || ($hours > $maxHours)){
-                echo "Error!";
-            }else{
-                if($hours > 40){
-                  $totalSalary =  $base * 40 + 1.5* $base *($hours - 40);
-
-                }else{
-                   $totalSalary = $base * $hours;
-                }
-            }
-            return "$name your total salary is $totalSalary.";
-            }
+    }
 }
 
-$employee1 = new Employee('Jim', 7.50, 35);
-$employee2 = new Employee('Michael', 8.20, 47);
-$employee3 = new Employee('Linda', 10.00, 73);
+$employee1 = new Employee( 7.50, 35);
+$employee2 = new Employee( 8.20, 47);
+$employee3 = new Employee( 10.00, 73);
 
-
+$employee1->main();
+echo PHP_EOL;
+$employee2->main();
+echo PHP_EOL;
+$employee3->main();
+echo PHP_EOL;
