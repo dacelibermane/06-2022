e56aa12fd34faa0a0203597f5c780c3cba165118
dacelibermane <dace.libermane@gmail.com>
2022-11-02 18:52:15 +0200

    modify function display_board

--- 07-arrays/05.php
+++ 07-arrays/05.php
@@ -1,26 +1,15 @@
 <?php
+$cells = [' ', ' ', ' ', ' ', ' ', ' ',' ', ' ', ' '];
 
-$cells = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-];
-$player = 'X';
-$activeCell = [0 => 0, 1 => 0];
-
-
-function display_board($cells)
+function display_board($cel)
 {
-    foreach($cells as $cell){
-        foreach($cell as $key =>$item){
-            echo " $key |";
-        }
-       echo "\n";
-       echo "---+---+---\n";
-    }
+    echo " $cel[0] | $cel[1] | $cel[2]\n";
+    echo "---+---+---\n";
+    echo " $cel[3] | $cel[4] | $cel[5]\n";
+    echo "---+---+---\n";
+    echo " $cel[6] | $cel[7] | $cel[8] \n";
 }
 
-$userX = readline("'X', choose your location (row, column): ");
-if()
-
 display_board($cells);
+
+
