9393593453e1b293dfe9a2d8192a8e05b3e36a66
dacelibermane <dace.libermane@gmail.com>
2022-11-02 22:16:46 +0200

    add for loop

--- 07-arrays/05.php
+++ 07-arrays/05.php
@@ -1,5 +1,4 @@
 <?php
-$cells = [' ', ' ', ' ', ' ', ' ', ' ',' ', ' ', ' '];
 
 function display_board($cel)
 {
@@ -10,6 +9,24 @@ function display_board($cel)
     echo " $cel[6] | $cel[7] | $cel[8] \n";
 }
 
+$cells = [' ', ' ', ' ', ' ', ' ', ' ',' ', ' ', ' '];
+
+$cellIndexes = [[]];
+$val = 0;
+
+for($i=0;$i<3;$i++){
+    for($j=0;$j<3;$j++){
+        $cellIndexes[$i][$j] = $val++;
+    }
+}
+
+$firstPlayer = 'O';
+$secondPlayer = 'X';
+$turn = $firstPlayer;
 display_board($cells);
 
+
+$inputRow = (int)readline("'X', choose your location (row, column): ");
+$inputColumn = (int)readline();
+$position = [[$inputRow, $inputColumn ]];
 
