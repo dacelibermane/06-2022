811ddf2095669d6bbaa5d598dd90b2774526c544
dacelibermane <dace.libermane@gmail.com>
2022-11-03 12:36:25 +0200

    change reference type

--- 07-arrays/05.php
+++ 07-arrays/05.php
@@ -1,15 +1,19 @@
 <?php
-
+$cells = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
+];
 function display_board($cel)
 {
-    echo " $cel[0] | $cel[1] | $cel[2]\n";
+    echo " {$cel[0][0]} | $cel[1] | $cel[2]\n";
     echo "---+---+---\n";
     echo " $cel[3] | $cel[4] | $cel[5]\n";
     echo "---+---+---\n";
     echo " $cel[6] | $cel[7] | $cel[8] \n";
 }
 
-$cells = [' ', ' ', ' ', ' ', ' ', ' ',' ', ' ', ' '];
+
 
 $cellIndexes = [[]];
 $val = 0;
@@ -20,13 +24,23 @@ for($i=0;$i<3;$i++){
     }
 }
 
-$firstPlayer = 'O';
-$secondPlayer = 'X';
-$turn = $firstPlayer;
+//$firstPlayer = 'O';
+//$secondPlayer = 'X';
+
+$playerSymbol = 'X';
+//$turn = $firstPlayer;
 display_board($cells);
 
+$userRow = (int)readline("'{$playerSymbol}', choose your location (row, column): ");
+$userColumn = (int)readline();
+$position = [[$userRow, $userColumn ]];
+//if()
 
-$inputRow = (int)readline("'X', choose your location (row, column): ");
-$inputColumn = (int)readline();
-$position = [[$inputRow, $inputColumn ]];
-
+//spēlē ir 9 laukumi, tādēļ kamēr nav aizpildīti visi laukumi spēle turpinās
+//for($i = 0; $i < 9; $i++){
+//
+//    do{
+//
+//
+//    }
+//}
