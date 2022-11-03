6a4c9b4dc1b06a3589fee8747fa394ebe8a774ff
dacelibermane <dace.libermane@gmail.com>
2022-11-03 19:44:36 +0200

    change switch

--- 08-flow-of-control/05.php
+++ 08-flow-of-control/05.php
@@ -2,38 +2,20 @@
 
 $userInput = str_split(strtolower(readline("Enter a word: ")));
 
-function phoneKeyPad($word): int
+function phoneKeyPad(array $word): int
 {
     $digit = 0;
     for ($i = 0; $i < count($word); $i++) {
         $char = $word[$i];
         switch ($char) {
-            case 'a': $digit .= 2; break;
-            case 'b': $digit .= 22; break;
-            case 'c': $digit .= 222; break;
-            case 'd': $digit .= 3; break;
-            case 'e': $digit .= 33; break;
-            case 'f': $digit .= 333; break;
-            case 'g': $digit .= 4; break;
-            case 'h': $digit .= 44; break;
-            case 'i': $digit .= 444; break;
-            case 'j': $digit .= 5; break;
-            case 'k': $digit .= 55; break;
-            case 'l': $digit .= 555; break;
-            case 'm': $digit .= 6; break;
-            case 'n': $digit .= 66; break;
-            case 'o': $digit .= 666; break;
-            case 'p': $digit .= 7; break;
-            case 'q': $digit .= 77; break;
-            case 'r': $digit .= 777; break;
-            case 's': $digit .= 7777; break;
-            case 't': $digit .= 8; break;
-            case 'u': $digit .= 88; break;
-            case 'v': $digit .= 888; break;
-            case 'w': $digit .= 9; break;
-            case 'x': $digit .= 99; break;
-            case 'y': $digit .= 999; break;
-            case 'z': $digit .= 9999; break;
+            case 'a': case 'b': case 'c': $digit .= 2; break;
+            case 'd': case 'e': case 'f': $digit .= 3; break;
+            case 'g': case 'h': case 'i': $digit .= 4; break;
+            case 'j': case 'k': case 'l': $digit .= 5; break;
+            case 'm': case 'n': case 'o': $digit .= 6; break;
+            case 'p': case 'q': case 'r': case 's': $digit .= 7; break;
+            case 't': case 'u': case 'v': $digit .= 8; break;
+            case 'w': case 'x': case 'y': case 'z': $digit .= 9; break;
         }
     }
     return $digit;
